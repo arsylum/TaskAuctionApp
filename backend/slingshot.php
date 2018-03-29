f39ccbd7e71424badb6264dc5ad23ddd904ddc78
caz <caz@razos>
2018-03-29 03:33:22 +0200

    add random_int() polyfill for older php

--- backend/slingshot.php
+++ backend/slingshot.php
@@ -14,6 +14,57 @@ header("Access-Control-Allow-Headers: Content-Type");
  
 if(!isset($_POST['action'])) { die('Invalid backend call!'); }
 
+
+// backport for PHP >= 5.1
+// https://secure.php.net/manual/en/function.random-int.php#119670
+if (!function_exists('random_int')) {
+    function random_int($min, $max) {
+        if (!function_exists('mcrypt_create_iv')) {
+            trigger_error(
+                'mcrypt must be loaded for random_int to work', 
+                E_USER_WARNING
+            );
+            return null;
+        }
+        
+        if (!is_int($min) || !is_int($max)) {
+            trigger_error('$min and $max must be integer values', E_USER_NOTICE);
+            $min = (int)$min;
+            $max = (int)$max;
+        }
+        
+        if ($min > $max) {
+            trigger_error('$max can\'t be lesser than $min', E_USER_WARNING);
+            return null;
+        }
+        
+        $range = $counter = $max - $min;
+        $bits = 1;
+        
+        while ($counter >>= 1) {
+            ++$bits;
+        }
+        
+        $bytes = (int)max(ceil($bits/8), 1);
+        $bitmask = pow(2, $bits) - 1;
+
+        if ($bitmask >= PHP_INT_MAX) {
+            $bitmask = PHP_INT_MAX;
+        }
+
+        do {
+            $result = hexdec(
+                bin2hex(
+                    mcrypt_create_iv($bytes, MCRYPT_DEV_URANDOM)
+                )
+            ) & $bitmask;
+        } while ($result > $range);
+
+        return $result + $min;
+    }
+}
+
+
 require('./db.conf.php');
 $db = new mysqli($creds['host'], $creds['user'], $creds['pass'], $creds['base']);
 if ($db->connect_error) { die('Connect Error (' . $db->connect_errno . ') ' . $db->connect_error); }
